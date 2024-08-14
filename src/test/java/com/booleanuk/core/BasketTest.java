b031fdac602a4b03d79d2c3ed0d7521d58831fff
TMajlu <thomasmajnie@gmail.com>
2024-08-14 14:43:29 +0200

    Finished the core exercises.

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -20,11 +20,13 @@ class BasketTest {
         Assertions.assertFalse(basket.remove("Nice bagel", 5));
     }
 
+
     @Test
     public void testCapacityExists(){
         Basket basket= new Basket(10);
         Assertions.assertTrue(basket.checkCapacity());
     }
+
 
     @Test
     public void testIncCapacityExists(){
@@ -32,6 +34,7 @@ class BasketTest {
         Assertions.assertTrue(basket.incCapacity(10));
     }
 
+    // testing User story 1
     @Test
     public void testAdd(){
         Basket basket= new Basket(10);
@@ -39,6 +42,7 @@ class BasketTest {
         Assertions.assertTrue(basket.add("Nice bagel", 5));
     }
 
+    // testing User story 2 & 5
     @Test
     public void testRemove(){
         Basket basket= new Basket(10);
@@ -47,6 +51,7 @@ class BasketTest {
         Assertions.assertFalse(basket.remove("Fine bagel", 1));
     }
 
+    // testing User story 3
     @Test
     public void testAddingtoFull(){
         Basket basket= new Basket(10);
@@ -54,6 +59,7 @@ class BasketTest {
         Assertions.assertFalse(basket.add("Nice bagel", 2));
     }
 
+    // testing User story 3
     @Test
     public void testCheckCapacity(){
         Basket basket= new Basket(10);
@@ -62,6 +68,7 @@ class BasketTest {
         Assertions.assertFalse(basket.checkCapacity());
     }
 
+    // testing User story 4
     @Test
     public void testIncCapacity(){
         Basket basket= new Basket(10);
